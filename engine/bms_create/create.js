43e31b07558c5a4afc1a0868ec8de77fc3e664e6
rainbowCN <viewsoft@yeah.net>
2014-10-24 17:55:50 +0800

    Added auto creating common code template.

---
+++ engine/bms_create/create.js
@@ -0,0 +1,25 @@
+/**
+ * @author James Yu (x.james.yu@gmail.com)
+ * @updated 2014.10.20
+ */
+
+//env args.
+var args = process.argv.slice(2);
+
+//load libs.
+var fs = require('fs'),
+handlebars = require('handlebars'),
+js_beautify = require('js-beautify').js_beautify,
+filed = require('filed');
+
+fs.readFile('templates/js_form_tpl.js', 'UTF-8', function(err, data){
+	var tpl = handlebars.compile(data);
+	fs.readFile('templates/meta.json', 'UTF-8', function(err, data){
+		var code = tpl(JSON.parse(data));
+		code = js_beautify(code);
+		var f = filed('./model.js');
+		f.write(code);
+		f.end();		
+	});
+});
+
--- implementation/index.php
+++ implementation/index.php
@@ -10,7 +10,7 @@ require '../engine/data/core/classesautoloader.php';
 
 $app = new \Slim\Slim();
 $app->response->headers->set('Content-Type', 'application/json');
-$app->add(new AuthMiddleware());
+//$app->add(new AuthMiddleware());
 $app->add(new FormatResponseMiddleware());
 
 $app->config('prefix', "bms_");
@@ -64,7 +64,7 @@ $app->get('/api/menu', function() use ($app) {
 		$result = array();
 		$result = file_get_contents("./static/config_data/menu.json");
 		if(preg_match('/^\xEF\xBB\xBF/',$result)){
-			//$result=substr($result,3);
+			$result=substr($result,3);
 		}
 		echo $result;
 	} catch(Exception $e) {
